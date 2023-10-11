34c2de7b5c12fda7584c375664f87e9dd4093c88
sandroanugrah <sandrotambunan174@gmail.com>
2023-10-11 19:27:42 +0700

    Menambahkan file

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hallo Dunia");
+        // attribut
+        int a, b;
+        int hasilJumlah, hasilKurang, hasilPerkalian;
+        a = 10;
+        b = 20;
+        // konstruktor
+        hasilJumlah = a + b;
+        hasilKurang = a - b;
+        hasilPerkalian = a * b;
+        System.out.print("hasilJumlahnya adalah\t: " + hasilJumlah + " \nhasilKurangnya adalah\t:" + hasilKurang
+                + " \nhasilPerkaliannya adalah:" + hasilPerkalian);
     }
 }
-
